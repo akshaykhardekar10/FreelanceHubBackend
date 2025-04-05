459ed164ee3383d59fbaee06d06eaf71da0cc428
akshaykhardekar10 <khardekarakshay33@gmail.com>
2025-04-05 20:40:29 +0530

    c

---
+++ FreelanceX2/src/main/java/com/example/FreelanceX2/Repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.example.FreelanceX2.Repository;
+
+import com.example.FreelanceX2.Model.Users;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<Users,String> {
+    Optional<Users> findByUsername(String username);
+    Optional<Users> findByEmail(String email);
+    Optional<Users> findById(String id);
+    boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
+
+
+}
